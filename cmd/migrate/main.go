6c09d5806dfe400d7d3597aa64cdea8a21ce4189
muna <victorycollins31@gmail.com>
2024-10-02 15:58:49 +0100

    💄 feat: Implement API server initialization with dynamic port configuration and error handling

--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -1,5 +1,15 @@
 package main 
 
+import (
+	"log"
+	"github.com/sikozonpc/ecom/cmd/api"
+)
+// setting up and running the server 
 func main () {
-	
+	server := api.NewAPIServer(fmt.Sprintf(":%s", configs.Envs.Port), db)
+if err := server.Run(); err != nil {
+	log.Fatal(err)
+
+}
+
 }
