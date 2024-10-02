f3f06711644649b5f482c718055402490abfb4bc
muna <victorycollins31@gmail.com>
2024-10-02 14:02:53 +0100

    🔥 feat: Initialize API server with database connection- Created APIserver struct with address and database fields.- Implemented NewAPIServer constructor function to initialize the server.- Added Run method to start the server and handle HTTP requests using mux router.

---
+++ cmd/api/api.go
@@ -0,0 +1,23 @@
+package api 
+
+import "database/sql"
+
+type APIserver struct {
+	addr string
+	db *sql.DB 
+}
+
+
+func NewAPIServer (addr string , db *sql.DB) *APIServer {
+	return &APIServer{
+		addr: addr,
+		db: db,
+	}
+}
+
+func (s *APIServer) Run() error{
+	router := mux.NewRouter()
+	subrouter := router.PathPrefix("/api/v1").Subrouter()
+
+	return http.ListenAndServe(s.addr, router)
+}
